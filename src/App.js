bebc3d5e119ad6e3e55cbda83262f6d739cdbe94
Majd Mawas <majosmws@gmail.com>
2022-09-11 15:07:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
 
 function App() {
+  const [data,setData] = React.useState()
+  const [newAdvice,setNewAdvice] = React.useState(true)
+  const [error,setError] = React.useState()
+  const [loading,setLoading] = React.useState(true)
+  
+  /* Fetching data from the API and setting the data to the state. */
+  React.useEffect(()=>{
+    if(newAdvice){
+    setNewAdvice(false)
+    fetch('https://api.adviceslip.com/advice')
+    .then((result) => result.json())
+    .then((actualData) => {
+      setData(actualData);
+      setError(null);
+    })
+    .catch((err) => {
+      setError(err.message);
+      setData(null);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+  }
+  },[newAdvice])
+
+  function handleClick(){
+    if(error&&loading)
+    return
+    setNewAdvice(true)
+  }
+  
+  if(data===undefined)
+  return
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className='advice-main'>
+        {/* {console.log("advice")} */}
+        <div className='advice-id'>ADVICE #{data.slip.id}</div>
+        <div className='advice-text'>	&ldquo;{data.slip.advice}&rdquo;</div>
+        <div className='line'></div>
+        <button className='new-advice' onClick={handleClick}></button>
+      </main>
     </div>
   );
 }
